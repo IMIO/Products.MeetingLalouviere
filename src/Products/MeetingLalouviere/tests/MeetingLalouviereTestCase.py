6f483a8fe84138eb512a50e31bcf13e2b876e38c
Gauthier Bastien <g.bastien@imio.be>
2014-08-06 07:59:59 +0000

    Do not define the test_suite as we heritate from MeetingCommunes that already defines it...

--- src/Products/MeetingLalouviere/tests/MeetingLalouviereTestCase.py
+++ src/Products/MeetingLalouviere/tests/MeetingLalouviereTestCase.py
@@ -35,12 +35,3 @@ class MeetingLalouviereTestCase(MeetingCommunesTestCase, MeetingLalouviereTestin
     """Base class for defining MeetingLalouviere test cases."""
 
     layer = MLL_TESTING_PROFILE_FUNCTIONAL
-
-
-# this is necessary to execute base test
-# test_tescasesubproduct_VerifyTestFiles from PloneMeeting
-def test_suite():
-    from unittest import TestSuite, makeSuite
-    suite = TestSuite()
-    suite.addTest(makeSuite(MeetingCommunesTestCase, prefix='test_testcasesubproduct_'))
-    return suite
