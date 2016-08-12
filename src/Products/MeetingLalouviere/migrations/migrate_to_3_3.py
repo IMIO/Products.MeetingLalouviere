fd962a5fecd9f9830579b9ec9b1aca6d4ce4827a
André Nuyens <andre.nuyens@imio.be>
2016-08-12 07:41:51 +0000

    adapt customized templates

--- src/Products/MeetingLalouviere/migrations/migrate_to_3_3.py
+++ src/Products/MeetingLalouviere/migrations/migrate_to_3_3.py
@@ -71,11 +71,11 @@ class Migrate_To_3_3(Migrator):
         logger.info('Done.')
 
     def run(self):
-        logger.info('Migrating to MeetingCommunes 3.3...')
+        logger.info('Migrating to MeetingLalouviere 3.3...')
         self._migrateItemDecisionReportTextAttributeOnConfigs()
         self._updateOnMeetingTransitionItemTransitionToTrigger()
         # reinstall so skins and so on are correct
-        self.reinstall(profiles=[u'profile-Products.MeetingCommunes:default', ])
+        self.reinstall(profiles=[u'profile-Products.MeetingLalouviere:default', ])
         self.finish()
 
 
@@ -84,7 +84,7 @@ def migrate(context):
     '''This migration function:
 
        1) Remove obsolete attribut 'itemDecisionReportText' from every meetingConfigs;
-       2) Reinstall Products.MeetingCommunes so skin and so on are correct.
+       2) Reinstall Products.MeetingLalouviere so skin and so on are correct.
     '''
     Migrate_To_3_3(context).run()
 # ------------------------------------------------------------------------------
