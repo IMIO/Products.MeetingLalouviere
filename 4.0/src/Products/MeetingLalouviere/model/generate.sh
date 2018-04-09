1e84828d90620f95ff7e01ae2643bb388610fd8e
Olivier Delaere <olivier.delaere@imio.be>
2018-04-09 08:25:28 +0000

    oups

--- 4.0/src/Products/MeetingLalouviere/model/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/srv/archgenxml/archgenxml-2.7/bin/archgenxml --cfg generate.conf MeetingLalouviere.zargo -o tmp
-
-# only keep workflows
-cp -rf tmp/profiles/default/workflows/meetingcollegelalouviere_workflow ../profiles/default/workflows
-cp -rf tmp/profiles/default/workflows/meetingitemcollegelalouviere_workflow ../profiles/default/workflows
-cp -rf tmp/profiles/default/workflows/meetingcouncillalouviere_workflow ../profiles/default/workflows
-cp -rf tmp/profiles/default/workflows/meetingitemcouncillalouviere_workflow ../profiles/default/workflows
-rm -rf tmp
