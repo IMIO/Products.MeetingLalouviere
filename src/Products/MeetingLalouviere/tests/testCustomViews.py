6cf5ee79df3a19d7f15671c68d797f38f398f80e
Antoine Duchene <antoine.duchene@imio.be>
2019-07-10 15:46:21 +0200

    unit tests for new methods in the helper view

---
+++ src/Products/MeetingLalouviere/tests/testCustomViews.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+#
+# File: testCustomMeeting.py
+#
+# Copyright (c) 2007-2013 by Imio.be
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+from Products.MeetingLalouviere.tests.MeetingLalouviereTestCase import MeetingLalouviereTestCase
+from Products.MeetingLalouviere.config import COUNCIL_MEETING_COMMISSION_IDS_2019
+from DateTime import DateTime
+
+
+class testCustomViews(MeetingLalouviereTestCase):
+    """
+        Tests the custom views
+    """
+
+    def setUp(self):
+        MeetingLalouviereTestCase.setUp(self)
+        # Commissions are only present in council so we use it instead of college
+        self.meetingConfig = self.meetingConfig2
+
+        # add MEETING_COMMISSION's categories
+        self.changeUser("admin")
+
+        commission_categories = []  # flatten COUNCIL_MEETING_COMMISSION_IDS_2019
+        for i in COUNCIL_MEETING_COMMISSION_IDS_2019:
+            if isinstance(i, tuple):
+                for j in i:
+                    commission_categories.append(j)
+            else:
+                commission_categories.append(i)
+        # create 1st-supplement for each cat
+        commission_categories = commission_categories + [
+            cat + "-1er-supplement" for cat in commission_categories
+        ]
+
+        for cat in commission_categories:
+            new_cat_id = self.meetingConfig.categories.invokeFactory(
+                "MeetingCategory", id=cat, title="commissionCat"
+            )
+            new_cat = getattr(self.meetingConfig.categories, new_cat_id)
+            new_cat.processForm()
+
+        # Create a meeting
+        self.changeUser("pmManager")
+        self.meeting = self.create("Meeting", date="2019/09/1 18:00:00")
+
+        # set the view
+        pod_template = self.meetingConfig.podtemplates.agendaTemplate
+        self.request.set("template_uid", pod_template.UID())
+        self.request.set("output_format", "odt")
+        view = self.meeting.restrictedTraverse("@@document-generation")
+        view()
+        self.helper = view.get_generation_context_helper()
+
+    def test_get_commission_items(self):
+        item = self.create("MeetingItem")
+        item.setCategory(COUNCIL_MEETING_COMMISSION_IDS_2019[0][0])  # First commission
+        item2 = self.create("MeetingItem")
+        item2.setCategory(
+            COUNCIL_MEETING_COMMISSION_IDS_2019[1][0]
+        )  # Second commission
+        item3 = self.create("MeetingItem")
+        item3.setCategory(
+            COUNCIL_MEETING_COMMISSION_IDS_2019[1][0]
+        )  # Second commission
+
+        item_s = self.create("MeetingItem")
+        item_s.setCategory(
+            COUNCIL_MEETING_COMMISSION_IDS_2019[0][0] + "-1er-supplement"
+        )  # First commission, 1st supp
+        item2_s = self.create("MeetingItem")
+        item2_s.setCategory(
+            COUNCIL_MEETING_COMMISSION_IDS_2019[1][0] + "-1er-supplement"
+        )  # Second commission, 1st supp
+        item3_s = self.create("MeetingItem")
+        item3_s.setCategory(
+            COUNCIL_MEETING_COMMISSION_IDS_2019[1][0] + "-1er-supplement"
+        )  # Second commission, 1st supp
+
+        items = (item, item2, item3, item_s, item2_s, item3_s)
+        for i in items:
+            self.presentItem(i)
+        item_uids = [i.UID() for i in items]
+
+        # Tests cases with type='normal'
+        comm_no1_items = self.helper.get_commission_items(item_uids, 1)
+        self.assertEquals(comm_no1_items[0], item)
+        self.assertEquals(len(comm_no1_items), 1)
+
+        comm_no2_items = self.helper.get_commission_items(item_uids, 2)
+        self.assertEquals(comm_no2_items[0], item2)
+        self.assertEquals(len(comm_no2_items), 2)
+
+        comm_no3_items = self.helper.get_commission_items(item_uids, 3)
+        self.assertEquals(len(comm_no3_items), 0)
+
+        # Tests cases with type='supp'
+        comm_no1_items = self.helper.get_commission_items(
+            item_uids, 1, type="supplement"
+        )
+        self.assertEquals(comm_no1_items[0], item_s)
+        self.assertEquals(len(comm_no1_items), 1)
+
+        comm_no2_items = self.helper.get_commission_items(
+            item_uids, 2, type="supplement"
+        )
+        self.assertEquals(comm_no2_items[0], item2_s)
+        self.assertEquals(len(comm_no2_items), 2)
+
+        comm_no3_items = self.helper.get_commission_items(
+            item_uids, 3, type="supplement"
+        )
+        self.assertEquals(len(comm_no3_items), 0)
+
+        # Tests cases with type='*', get all items for each commission regardless of type
+        comm_no1_items = self.helper.get_commission_items(item_uids, 1, type="*")
+        self.assertEquals(comm_no1_items[0], item)
+        self.assertEquals(len(comm_no1_items), 2)
+
+        comm_no2_items = self.helper.get_commission_items(item_uids, 2, type="*")
+        self.assertEquals(comm_no2_items[2], item2_s)
+        self.assertEquals(len(comm_no2_items), 4)
+
+        comm_no3_items = self.helper.get_commission_items(item_uids, 3, type="*")
+        self.assertEquals(len(comm_no3_items), 0)
+
+    def test_format_commission_pre_meeting_date(self):
+        self.meeting.setPreMeetingDate(DateTime(2019, 9, 1, 4, 20, 0))
+        self.meeting.setPreMeetingPlace("Pays des bisounours")
+        self.meeting.setPreMeetingDate_2(DateTime(2019, 9, 2, 4, 20, 0))
+        self.meeting.setPreMeetingPlace_2("Sur la Lune")
+
+        self.assertEquals(
+            self.helper.format_commission_pre_meeting_date(1),
+            "(Sunday 01 september 2019 (04H20), Pays des bisounours)",
+        )
+        self.assertEquals(
+            self.helper.format_commission_pre_meeting_date(2),
+            "(Monday 02 september 2019 (04H20), Sur la Lune)",
+        )
+
+    def test_get_commission_assembly(self):
+        self.meeting.setPreMeetingAssembly("myAssembly")
+        self.meeting.setPreMeetingAssembly_2("myAssembly 2")
+        self.assertEqual(
+            self.meeting.getPreMeetingAssembly(), self.helper.get_commission_assembly(1)
+        )
+        self.assertEqual(
+            self.meeting.getPreMeetingAssembly_2(),
+            self.helper.get_commission_assembly(2),
+        )
