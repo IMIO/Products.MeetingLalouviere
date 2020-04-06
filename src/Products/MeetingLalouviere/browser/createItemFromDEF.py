8f980b764c3ced0515b9422b3416ea41e98cabf0
Olivier Delaere <olivier.delaere@imio.be>
2020-04-06 14:38:10 +0200

    Fix organization management in def views

--- src/Products/MeetingLalouviere/browser/createItemFromDEF.py
+++ src/Products/MeetingLalouviere/browser/createItemFromDEF.py
@@ -3,6 +3,7 @@ from AccessControl import Unauthorized
 from Acquisition import aq_inner
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from Products.PloneMeeting.utils import org_id_to_uid
 
 
 class CreateItemFromDEFView(BrowserView):
@@ -17,22 +18,23 @@ class CreateItemFromDEFView(BrowserView):
         self.itemDescription = request.get('defdescription', '')
         self.itemMotivation = request.get('defmotivation', '')
         self.itemDecision = request.get('defdecision', '')
-        #every decisions coming out from the DEF intranet are about
-        #def-gestion-administrative-personnel
-        self.itemProposingGroup = "def-gestion-administrative-personnel"
+        # every decisions coming out from the DEF intranet are about
+        # def-gestion-administrative-personnel
+        self.itemProposingGroup = org_id_to_uid('def-gestion-administrative-personnel')
 
     def mayUserAddItem(self):
         """
           The user must be connected
         """
-        #the user must be logged in
+        # the user must be logged in
         context = aq_inner(self.context)
         mtool = getToolByName(context, 'portal_membership')
-        #the user must be creator for "def-gestion-administrative-personnel"
+        # the user must be creator for "def-gestion-administrative-personnel"
         if mtool.isAnonymousUser():
             raise Unauthorized
         member = mtool.getAuthenticatedMember()
-        if not "def-gestion-administrative-personnel_creators" in member.getGroups():
+        creator_group = "{}_creators".format(self.itemProposingGroup)
+        if creator_group not in member.getGroups():
             raise Unauthorized
         return True
 
@@ -41,20 +43,20 @@ class CreateItemFromDEFView(BrowserView):
           Proceed with item creation
         """
         context = aq_inner(self.context)
-        #raise Unauthorized if the user can not add an item for the DEF
+        # raise Unauthorized if the user can not add an item for the DEF
         self.mayUserAddItem()
 
         member = context.restrictedTraverse('@@plone_portal_state').member()
         meetingFolder = getattr(member.getHomeFolder().mymeetings, 'meeting-config-college')
         data = {
             'title': self.getItemTitle(),
-            'proposingGroup': self.getItemProposingGroup(),
+            'proposingGroup': self.itemProposingGroup,
         }
         newitemid = meetingFolder.invokeFactory('MeetingItemCollege',
                                                 id=context.generateUniqueId('MeetingItemCollege'),
                                                 **data)
         newitem = getattr(meetingFolder, newitemid)
-        #special handling for text/html decision field
+        # special handling for text/html decision field
         newitem.setDescription(self.getItemDescription(), contenttype='text/html')
         newitem.setMotivation(self.getItemMotivation(), contenttype='text/html')
         newitem.setDecision(self.getItemDecision(), contenttype='text/html')
@@ -85,9 +87,3 @@ class CreateItemFromDEFView(BrowserView):
           Returns the item description
         """
         return base64.b64decode(self.itemDescription)
-
-    def getItemProposingGroup(self):
-        """
-          Returns the item proposing group
-        """
-        return self.itemProposingGroup
--- src/Products/MeetingLalouviere/browser/logUserFromDEF.py
+++ src/Products/MeetingLalouviere/browser/logUserFromDEF.py
@@ -15,15 +15,15 @@ class LogUserFromDEFView(BrowserView):
         super(BrowserView, self).__init__(context, request)
         self.context = context
         self.request = request
-        #we NEED a defurl and we check that we are correctly redirected to the DEF
-        #to avoid this view being used for fishing
+        # we NEED a defurl and we check that we are correctly redirected to the DEF
+        # to avoid this view being used for fishing
         defurl = self.request.get('defurl', None)
         self.defurl = None
         if defurl:
             try:
                 self.defurl = base64.b64decode(defurl)
                 if not self.defurl.startswith(DEFURL):
-                    raise
+                    raise Unauthorized
             except:
                 raise Unauthorized
         else:
@@ -37,9 +37,9 @@ class LogUserFromDEFView(BrowserView):
         context = aq_inner(self.context)
         mtool = getToolByName(context, 'portal_membership')
         if mtool.isAnonymousUser():
-            #raise an unauthorized so the login page is shown to the user
+            # raise an unauthorized so the login page is shown to the user
             raise Unauthorized
         else:
             request = aq_inner(self.request)
-            #to avoid args mismatches in the url, the def back url is passed in base64
+            # to avoid args mismatches in the url, the def back url is passed in base64
             request.RESPONSE.redirect(self.defurl)
