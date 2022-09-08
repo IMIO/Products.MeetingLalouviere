2e8fc9373b4e544c056eff616f1de413e1229054
odelaere <odelaere@users.noreply.github.com>
2022-09-08 13:32:41 +0200

    Fix test_pm_SendMailIfRelevantIsPermission

--- src/Products/MeetingLalouviere/tests/testUtils.py
+++ src/Products/MeetingLalouviere/tests/testUtils.py
@@ -30,7 +30,20 @@ from Products.MeetingLalouviere.tests.MeetingLalouviereTestCase import (
 
 class testUtils(MeetingLalouviereTestCase, mctu):
     """ """
+    def _default_permission_mail_recipents(self):
+        return [u'M. Budget Impact Editor <budgetimpacteditor@plonemeeting.org>',
+                u'M. PMCreator One <pmcreator1@plonemeeting.org>',
+                u'M. PMCreator One bee <pmcreator1b@plonemeeting.org>',
+                u'M. PMObserver One <pmobserver1@plonemeeting.org>',
+                u'M. PMReviewer One <pmreviewer1@plonemeeting.org>',
+                u'M. Power Observer1 <powerobserver1@plonemeeting.org>',
+                u'Site administrator <siteadmin@plonemeeting.org>',
+                u'pmFollowup1 <user@plonemeeting.org>']
 
+    def _modify_permission_mail_recipents(self):
+        return [u'M. PMCreator One <pmcreator1@plonemeeting.org>',
+                u'M. PMCreator One bee <pmcreator1b@plonemeeting.org>',
+                u'Site administrator <siteadmin@plonemeeting.org>']
 
 def test_suite():
     from unittest import TestSuite, makeSuite
